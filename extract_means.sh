69b7e44c8ba3050417f0c47257049cea7801fe07
Hanna Nowicka <hnowicka@fmrib.ox.ac.uk>
2019-03-02 15:43:24 +0100

    added removing temp dir and saving output to the original data dir

--- extract_means.sh
+++ extract_means.sh
@@ -5,8 +5,8 @@
 # Hanna Nowicka, Brainhack Warsaw, March 2019
 
 
-dir='..'
-tmp_dir='temp'
+dir='..' # where your T1s data is
+tmp_dir='temp' # just temp dir for auxiliary files 
 
 mkdir ${dir}/${tmp_dir}
 
@@ -17,14 +17,15 @@ for i in $(seq 134);
         # single out the ROI out of the parcellation:
         fslmaths ${dir}/t1_gm_parc.nii.gz -thr $(echo "$i - 0.5" | bc)  -uthr  $(echo "$i + 0.5" | bc) -bin ${dir}/${tmp_dir}/tmp_region.nii.gz
 
-
         # extract mean from that T1 masked by the single region:
         mean=`fslstats ${dir}/t1_brain.nii.gz  -k ${dir}/${tmp_dir}/tmp_region.nii.gz  -M | awk '{print $1}'`
         # extract st deviation:
         stdev=`fslstats ${dir}/t1_brain.nii.gz  -k ${dir}/${tmp_dir}/tmp_region.nii.gz  -S | awk '{print $1}'`
-        echo -n $mean >> ${dir}/${tmp_dir}/means.txt
-        echo -n "," >> ${dir}/${tmp_dir}/means.txt
-        echo $stdev  >> ${dir}/${tmp_dir}/stdevs.txt
-        echo -n "," >> ${dir}/${tmp_dir}/stdevs.txt
+        echo -n $mean >> ${dir}/means.txt
+        echo -n "," >> ${dir}/means.txt
+        echo $stdev  >> ${dir}/stdevs.txt
+        echo -n "," >> ${dir}/stdevs.txt
+
+        rm -r ${dir}/${tmp_dir}
 
 done
