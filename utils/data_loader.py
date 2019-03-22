b607b47526c891c29e56d0b1f6ac02cfbcf2c63c
xgoogax <agoogia@gmail.com>
2019-03-22 23:44:51 +0100

    final predictions

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np 
 
 class Dataset:
-	def __init__(self, filenames, drop_cols, label_column):
+	def __init__(self, filenames, drop_cols, label_column, test=False):
 		self.files = filenames
 		self.drop_cols = drop_cols
 		self.label_column = label_column
+		self.test = test
+		self.subjects = None
 		self.files_no_headers = ['data/training_data_means.csv', 'data/validation_data_means.csv', 'data/training_data_stdevs.csv', \
-		'data/validation_data_stdevs.csv', 'data/training_data_entropy.csv', 'data/validation_data_entropy.csv']
+		'data/validation_data_stdevs.csv', 'data/training_data_entropy.csv', 'data/validation_data_entropy.csv', \
+			'data/testing_data_means.csv', 'data/testing_data_entropy.csv',  'data/testing_data_stdev.csv']
 		self.final_dataset, self.meta_data = self.load_datasets()
 		self.data, self.labels = self.split_data_labels()
 
@@ -30,22 +33,32 @@ class Dataset:
 			meta_data[dataset] = {
 				'columns': list(df.columns)
 			}
+			if 'btsv01' in dataset:
+				df = df.drop([0], axis=0)
 			if i==0:
 				final_dataset = df
-			else:
+			elif i==1 :#or self.test:
 				left_merge_value = 'subject' if 'subject' in final_dataset.columns else 'subjectkey'
 				right_merge_value = 'subject' if 'subject' in df.columns else 'subjectkey'
 				final_dataset = pd.merge(final_dataset, df, left_on=left_merge_value, right_on=right_merge_value)
-		final_dataset =  final_dataset.drop([0], axis=0) #we are dropping this column with descriptions
+			else:
+				left_merge_value = 'subject' if 'subject' in final_dataset.columns else 'subjectkey'
+				right_merge_value = 'subject' if 'subject' in df.columns else 'subjectkey'
+				final_dataset = pd.merge(final_dataset, df, left_on=left_merge_value, right_on=right_merge_value, how='outer')
+		# final_dataset =  final_dataset.drop([0], axis=0) #we are dropping this column with descriptions
 		if self.drop_cols is not None: 
 			final_dataset = self.drop_columns(final_dataset)
 		meta_data['final_dataset'] = {
 			'columns': list(final_dataset.columns)
 		}
+		final_dataset = final_dataset.fillna(final_dataset.mean())
 		return final_dataset, meta_data
 
 	def split_data_labels(self):
+		if self.test: 
+			self.final_dataset[self.label_column] = 0
 		y = self.final_dataset[self.label_column].values.astype(float)
+		self.subjects = self.final_dataset['subjectkey']
 		X = self.final_dataset.drop([self.label_column, 'subject', 'subjectkey'], axis=1).values.astype(float)
 		self.meta_data['final_dataset']['columns'] = [x for x in self.meta_data['final_dataset']['columns'] if x not in [self.label_column, 'subject', 'subjectkey']]
 		return X, y
